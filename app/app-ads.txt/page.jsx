a14e3f21cc20129da18e5a5a4aecbaa1f023d2f4
Md. Al Mamun <almamun.coder@gmail.com>
2025-10-27 21:50:09 +0600

    New Route Set

--- app/app-ads.txt/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function AdsTxt() {
-  return (
-    <p>google.com, pub-7940426000750468, DIRECT, f08c47fec0942fa0</p>
-  );
-}
---
+++ app/app-ads.txt/route.js
@@ -0,0 +1,9 @@
+export async function GET() {
+  const content = 'google.com, pub-7940426000750468, DIRECT, f08c47fec0942fa0';
+  
+  return new Response(content, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}

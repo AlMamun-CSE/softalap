b03bb6d33b30f0a4abf9ae80ae1e766771f162ae
Md. Al Mamun <almamun.coder@gmail.com>
2025-10-28 11:09:46 +0600

    completed

--- app/app-ads.txt/route.js
+++ app/app-ads.txt/route.js
@@ -1,9 +1,9 @@
-export async function GET() {
-  const content = 'google.com, pub-7940426000750468, DIRECT, f08c47fec0942fa0';
+// export async function GET() {
+//   const content = 'google.com, pub-7940426000750468, DIRECT, f08c47fec0942fa0';
   
-  return new Response(content, {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  });
-}
+//   return new Response(content, {
+//     headers: {
+//       'Content-Type': 'text/plain',
+//     },
+//   });
+// }
